e603876b66a32352eaa89999719ab21d362e0d1d
Alex <opleno.dev@gmail.com>
2022-11-27 00:18:38 +0100

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session); // (session) is a parameter for the import
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -49,24 +51,21 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
-    var err = new Error("You are not authenticated");
-    err.status = 401;
+  if (!req.user) {
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
