770eab2257721d27d423d02aeeb0a6b5ac908e07
Alex <opleno.dev@gmail.com>
2022-11-28 19:16:51 +0100

    Assignment 4 (v2.0)

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -12,12 +12,28 @@ const favoriteRouter = express.Router();
 
 favoriteRouter.use(express.static(__dirname + "/public"));
 
+////////////////////
+// auxiliar functions
+//////////////////////
+
 // cannot use indexOf so I'll use this:
 function arrayObjectIndexOf(myArray, property, searchTerm) {
   for (var i = 0, len = myArray.length; i < len; i++) {
     if (myArray[i][property].equals(new ObjectId(searchTerm))) return i;
   }
   return -1;
+}
+
+// Javascript program to check if all array elements are distinct
+function areDistinct(arr) {
+  console.log("Array distinct: ", arr)
+  let n = arr.length;
+  // Put all array elements in a map
+  let s = new Set();
+  for (let i = 0; i < n; i++) {
+    s.add(arr[i]);
+  } // If all elements are distinct, size of set should be same array.
+  return s.size == arr.length;
 }
 
 favoriteRouter
@@ -43,15 +59,11 @@ favoriteRouter
   })
 
   .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    console.log("Entering Post /favorite");
-
     // Find the Favorite document associated to the userId
     Favorite.find({ userId: req.user._id })
       .then(
         (fav) => {
           if (fav.length === 1) {
-            console.log("Fav doc was found: ", fav);
-
             req.body.forEach((dishTuple) => {
               // Find if the dishId is already in the dishes array of the Favorite document
               if (
@@ -61,10 +73,8 @@ favoriteRouter
                   "Dish " + dishTuple._id + " already present in this favorite"
                 );
               }
-              // dishTuple._id is not there yet, should be added to the dishes array
+              // dishTuple._id is not there yet, proceed to add it to the dishes array
               else {
-                console.log("dish not found in Fav doc, proceed to add it");
-
                 Favorite.updateOne(
                   { userId: req.user._id },
                   { $push: { dishes: { _id: dishTuple._id } } }
@@ -87,21 +97,28 @@ favoriteRouter
           } else if (fav.length === 0) {
             console.log("updateOne could not find referred favorite");
 
-            Favorite.create({
-              userId: req.user._id,
-              dishes: req.body, // [{ _id: req.params.dishId }],
-            })
-              .then(
-                (fav) => {
-                  console.log("Favorite created: ", fav);
+            // Checking there are no repeated ids in the body of the request
+            if (areDistinct(req.body.map(e => e._id))) {
+              Favorite.create({
+                userId: req.user._id,
+                dishes: req.body,
+              })
+                .then(
+                  (fav) => {
+                    console.log("Favorite created: ", fav);
 
-                  res.statusCode = 201;
-                  res.setHeader("Content-Type", "application/json");
-                  res.json(fav);
-                },
-                (err) => next(err)
-              )
-              .catch((err) => next(err));
+                    res.statusCode = 201;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(fav);
+                  },
+                  (err) => next(err)
+                )
+                .catch((err) => next(err));
+            } else {
+              var err = new Error("Some elements of the body are repeated");
+              err.status = 400;
+              return next(err);
+            }
           } else {
             return next(err);
           }
